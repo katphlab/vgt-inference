faeb334352b61f3b9165e91052b64d150d797596
lalit@pc-wsl <lalit@pc-wsl>
2024-01-07 12:42:28 +0800

    Roll back initialization

--- vgt_inference/inference.py
+++ vgt_inference/inference.py
@@ -6,24 +6,14 @@ from .predictor import DefaultPredictor
 
 
 class VGTPredictor:
-    def __init__(
-        self, config_path: str, model_weights_path: str, device: str, hf_token: str
-    ) -> None:
+    def __init__(self, config_path: str, model_weights_path: str, device: str, hf_token: str) -> None:
         cfg = get_cfg()
         add_vit_config(cfg)
         cfg.merge_from_file(config_path)
 
         cfg.MODEL.DEVICE = device
 
-        self.cfg = cfg
-        self.hf_token = hf_token
-        self.model_weights_path = model_weights_path
-
-        self.tokenizer = None
-        self.predictor = None
-
-    def load(self) -> None:
         self.tokenizer = BrosTokenizer.from_pretrained(
-            "thewalnutaisg/bros-base-uncased", token=self.hf_token
+            "thewalnutaisg/bros-base-uncased", token=hf_token
         )
-        self.predictor = DefaultPredictor(self.cfg, self.model_weights_path)
+        self.predictor = DefaultPredictor(cfg, model_weights_path)
